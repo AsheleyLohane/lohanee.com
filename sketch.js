00f28c41c1b18e900ec571f42fd59e35f06fdbe0
AsheleyLohane <meloasheley25@gmail.com>
2024-09-06 12:17:07 -0300

    SOCORRO

---
+++ sketch.js
@@ -0,0 +1,61 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+    
+    
+    stroke("yellow")
+    fill("purple")
+    
+    //console.log(mouseIsPressed);
+    
+    if(mouseIsPressed) {
+    circle (mouseX, mouseY, 20,35);
+    }
+  
+    
+  }
+  
+  let cor;
+  let posicaoHorizontal; // x
+  let posicaoVertical; // y
+  
+  function setup() {
+    createCanvas(400, 400);
+     background(color(189,236,182));
+    cor = color(random(0,255), random(0,255), random(0,255));
+    posicaoHorizontal = 200;
+    posicaoVertical = 200;
+  }
+  
+  
+  function draw() {
+    
+    fill(cor);
+    circle(posicaoHorizontal,posicaoVertical,50);
+   
+    
+    
+    if (mouseX < posicaoHorizontal){
+      posicaoHorizontal =  posicaoHorizontal - 1;
+    }
+    
+    if (mouseX > posicaoHorizontal){
+      posicaoHorizontal =  posicaoHorizontal + 1;
+    }
+    
+    if (mouseY < posicaoVertical){
+      posicaoVertical--;
+    }
+      
+    if (mouseY > posicaoVertical){
+      posicaoVertical++;
+    }
+    
+    if (mouseIsPressed){
+      cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+    }
+      
+  }
